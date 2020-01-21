4eacfbb23aad54308bd0df7855baad408e4b33e5
Jacob-Brink <jacobpeterbrink@gmail.com>
2020-01-21 11:13:44 -0500

    Revert "created x-axis selector"
    This reverts commit 7628199ccebddcbcada3140a1ccf27f420906dc2.

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
+<<<<<<< HEAD
 const dataPromise = d3.csv('data/simple_2007_2019.csv')
 .then(function(d) {
     console.log(d);
     setup(d);
+=======
+const data = d3.csv('data/simple_2007_2019.csv')
+.then(function(data) {
+    console.log(data);
+>>>>>>> parent of 7628199... created x-axis selector
 })
 .catch(function(error){
     console.log(error);
 });
 
+<<<<<<< HEAD
 function cleaupData(d) {
     
 }
@@ -53,3 +60,21 @@ d3.select('#slider')
   .attr('transform', 'translate(30,30)')
   .call(slider)
 
+=======
+var margin = {top: 30, right: 30, bottom: 70, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#advait")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+
+var slider = d3.select("#tom")
+.sliderHorizontal()
+>>>>>>> parent of 7628199... created x-axis selector
